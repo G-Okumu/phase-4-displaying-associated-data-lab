4f8276ad40db5aacf32b3f898afb425f5406b8fa
George-Okumu <gokumu12@gmail.com>
2022-06-18 11:39:43 +0300

    complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_user_not_found
+
+    def show
+        user = User.find(params[:id])
+        render json: user, include: :items
+    end
+
+    private
+    def render_user_not_found
+        render json: { error: "user not found" }, status: :not_found
+    end
 end
